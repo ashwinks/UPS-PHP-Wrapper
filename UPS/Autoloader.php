0b0b5bda2050fd72100c56512e0ca03947101d8e
Ashwin Surajbali <ashwin@teedot.co>
2014-04-17 23:13:29 -0400

    removed unnecessary comments

--- UPS/Autoloader.php
+++ UPS/Autoloader.php
@@ -2,29 +2,19 @@
 
 namespace UPS;
 
-/**
- * Autoloads UPS classes
- *
- * @package ups
- */
 class Autoloader {
-    /**
-     * Register the autoloader
-     */
-    public static function register() {
+
+    public static function register()
+    {
         spl_autoload_register(array(new self, 'autoload'));
     }
 
-    /**
-     * Autoloader
-     *
-     * @param   string
-     */
-    public static function autoload($class) {
-        if (0 !== strpos($class, 'UPS\\')) {
+    public static function autoload($class)
+    {
+        if (0 !== strpos($class, 'UPS\\')){
             echo $class . ' naat found';
             return;
-        } else if (file_exists($file = dirname(__FILE__) . '/' . preg_replace('{^UPS\\\}', '', $class) . '.php')) {
+        }else if (file_exists($file = dirname(__FILE__) . '/' . preg_replace('{^UPS\\\}', '', $class) . '.php')){
             echo $file;
             require $file;
         }
